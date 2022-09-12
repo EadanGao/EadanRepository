94e28ee89ae32f09626745920f14034b81a7a808
Yidan <Yidan25@users.noreply.github.com>
2022-09-12 20:22:11 +0200

    time complexity O(n), space complexity O(1)

--- C codes/LeetCode 667 Beautiful Arrangement II.c
+++ C codes/LeetCode 667 Beautiful Arrangement II.c
@@ -1,25 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
 int* constructArray(int n, int k, int* returnSize){
-    int * ans = (int *) malloc(n * sizeof(int));
-    int i = 1;
-    *returnSize = 0;
-    while(i < n - k) ans[(*returnSize)++] = i++;
-
-    for(int i = n - k, j = n; i <= j; i++, j--){
-        ans[(*returnSize)++] = i;
-        if(i != j) ans[(*returnSize)++] = j;
+    int * num = (int *) malloc(n * sizeof(int));
+    
+    (*returnSize) = 0;
+    while((*returnSize) < n - k) num[(*returnSize)++] = (*returnSize);
+    int i = *returnSize + 1, j = n;;
+    while((*returnSize) < n){
+        num[(*returnSize)++] = j--;
+        if((*returnSize) < n) num[(*returnSize)++] = i++;
     }
-    *returnSize = n;
-    return ans;
-
+    return num;
 }
+
+
+// int* constructArray(int n, int k, int* returnSize){
+//     int * ans = (int *) malloc(n * sizeof(int));
+//     int i = 1;
+//     *returnSize = 0;
+//     while(i < n - k) ans[(*returnSize)++] = i++;
+
+//     for(int i = n - k, j = n; i <= j; i++, j--){
+//         ans[(*returnSize)++] = i;
+//         if(i != j) ans[(*returnSize)++] = j;
+//     }
+//     *returnSize = n;
+//     return ans;
+
+// }
+
 
 
 int main(){
+    int n, k, returnSize;
+    int a = scanf("%d %d", &n, &k);
+    int * num = NULL;
+    num = constructArray(n, k, &returnSize);
+    // for(int i = 0; i < returnSize; i++) printf("%d ", num[i]);
+    // printf("\n");
     return 0;
 }
+
